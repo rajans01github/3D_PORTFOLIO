9fa94ac198729e9461cb5e1c5829dd7d5ec3560a
RAJAN PANDEY <rajankumar221002@gmail.com>
2025-07-18 10:26:42 +0530

    🔥About changed.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,32 +1,31 @@
-import React from 'react'
-import { Tilt } from 'react-tilt'
-import {motion} from 'framer-motion';
+import { Tilt } from 'react-tilt';
+import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
 import { services } from '../constants';
-import { fadeIn,textVariant } from '../utils/motion';
+import { fadeIn, textVariant } from '../utils/motion';
 import { SectionWrapper } from '../hoc';
 
+// eslint-disable-next-line react-refresh/only-export-components, react/prop-types
 const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className='xs:w-[250px] w-full'>
+  <Tilt
+    className='xs:w-[250px] w-full'
+    options={{
+      max: 45,
+      scale: 1,
+      speed: 450,
+    }}
+  >
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
       className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
     >
-      <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
-      >
+      <div className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'>
         <img
           src={icon}
           alt='web-development'
           className='w-16 h-16 object-contain'
         />
-
         <h3 className='text-white text-[20px] font-bold text-center'>
           {title}
         </h3>
@@ -35,24 +34,24 @@ const ServiceCard = ({ index, title, icon }) => (
   </Tilt>
 );
 
+// eslint-disable-next-line react-refresh/only-export-components
 const About = () => {
-  return(
+  return (
     <>
-      <motion.dev varients={textVariant()}>
-        <p className={styles.sectionSubText}
-        >Introduction</p>
-        <h2 className={styles.sectionHeadText}> Overview. </h2>
-      </motion.dev>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
 
       <motion.p
-        variants={fadeIn("","",0.1,1)}
-        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
+        Im a skilled software developer with experience in TypeScript and
         JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
+        Three.js. Im a quick learner and collaborate closely with clients to
         create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        real-world problems. Lets work together to bring your ideas to life!
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
@@ -61,8 +60,8 @@ const About = () => {
         ))}
       </div>
     </>
+  );
+};
 
-  )
-}
-
-export default SectionWrapper(About,"about")
+// eslint-disable-next-line react-refresh/only-export-components
+export default SectionWrapper(About, "about");
